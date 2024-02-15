4e2e441a11c3284aeca5867fcf76c2c3dbf9369a
rajneeshyadav322 <rajneeshyadav.ry231@gmail.com>
2024-02-15 15:46:09 +0530

    refactored playlist item component

---
+++ src/components/playlist-item/PlaylistItem.jsx
@@ -0,0 +1,29 @@
+import React, { useContext } from 'react'
+import { PlayerContext } from '../../context/playerContext';
+
+const PlaylistItem = ({ provided, data }) => {
+
+    const { id, title, source } = data
+    const { video, setVideo } = useContext(PlayerContext);
+
+    const handleVideo = (source) => setVideo(source)
+
+    return (
+        <div
+            ref={provided.innerRef}
+            {...provided.dragHandleProps}
+            {...provided.draggableProps}
+        >
+            <div onClick={() => handleVideo(source)} key={id} className={`p-4 my-2 space-x-2 cursor-pointer flex ${video === source ? "bg-slate-200" : "bg-slate-100"} rounded-md`}>
+                <img src={'/play.png'} className='w-6 h-6' />
+                <div>
+                    <p className='text-sm'>
+                        {title}
+                    </p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default PlaylistItem
--- src/components/playlist/Playlist.jsx
+++ src/components/playlist/Playlist.jsx
@@ -1,15 +1,11 @@
-import React, { useContext, useState } from "react";
+import React, { useState } from "react";
 import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
 import { playlist } from '../../utils/playlist'
-import { PlayerContext } from '../../context/playerContext';
+import PlaylistItem from "../playlist-item/PlaylistItem";
 
 const Playlist = () => {
 
-  // React state to track order of items
   const [itemList, setItemList] = useState(playlist);
-  const { video, setVideo } = useContext(PlayerContext);
-
-  const handleVideo = (source) => setVideo(source)
 
   const handleDrop = (droppedItem) => {
     if (!droppedItem.destination) return;
@@ -19,11 +15,9 @@ const Playlist = () => {
     setItemList(updatedList);
   };
 
-
-
   return (
     <div className="border-2  tracking-tight rounded-lg">
-      <p className='text-xl tracking-tight text-gray-600 px-4 rounded-t-md py-2 w-full bg-slate-100'>Playlist 
+      <p className='text-xl tracking-tight text-gray-600 px-4 rounded-t-md py-2 w-full bg-slate-100'>Playlist
         <span className="text-[14px] ml-2">(Drag & drop videos for reordering)</span>
       </p>
       <DragDropContext onDragEnd={handleDrop}>
@@ -37,20 +31,7 @@ const Playlist = () => {
               {itemList.map((item, index) => (
                 <Draggable key={item.id} draggableId={item.id} index={index}>
                   {(provided, snapshot) => (
-                    <div
-                      ref={provided.innerRef}
-                      {...provided.dragHandleProps}
-                      {...provided.draggableProps}
-                    >
-                      <div onClick={() => handleVideo(item.source)} key={item.id} className={`p-4 my-2 space-x-2 cursor-pointer flex ${video === item.source ? "bg-slate-200" : "bg-slate-100"} rounded-md`}>
-                        <img src={'/play.png'} className='w-6 h-6' />
-                        <div>
-                          <p className='text-sm'>
-                            {item.title}
-                          </p>
-                        </div>
-                      </div>
-                    </div>
+                    <PlaylistItem provided={provided} data={item} />
                   )}
                 </Draggable>
               ))}
